683ef407bfdc006cf8f723f1ecc21d70f0657906
Ethan Heil <ethan.heil@mymail.champlain.edu>
2019-02-13 20:15:31 -0500

    Adding shoot cooldown

--- VerticalShooter/Assets/Scripts/ShootLaser.cs
+++ VerticalShooter/Assets/Scripts/ShootLaser.cs
@@ -7,6 +7,9 @@ public class ShootLaser : MonoBehaviour
 
     public GameObject laser;
     public Transform laserSpawn;
+    public float cooldown = 0.25f;
+
+    bool canFire = true;
 
     // Start is called before the first frame update
     void Start()
@@ -22,14 +25,22 @@ public class ShootLaser : MonoBehaviour
 
     void CheckInput()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKey(KeyCode.Space) && canFire)
         {
             FireLaser();
+            canFire = false;
+            StartCoroutine(ShootCooldown(cooldown));
         }
     }
 
     void FireLaser()
     {
         GameObject newLaser = Instantiate(laser, laserSpawn.position, laserSpawn.rotation);
+    }
+
+    IEnumerator ShootCooldown(float time)
+    {
+        yield return new WaitForSeconds(time);
+        canFire = true;
     }
 }
