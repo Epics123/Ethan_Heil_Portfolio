2acd78a500355a1365b363978b3739c62b5ce35f
Ethan Heil <ethan.heil@mymail.champlain.edu>
2019-02-13 19:56:00 -0500

    Adding laser movement and delayed destruction

---
+++ VerticalShooter/Assets/Scripts/Laser.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+
+    public float speed = 5f;
+    public int lifeSpan = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(DelayedDestroyLaser(lifeSpan));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(0, speed * Time.deltaTime, 0));
+    }
+
+    IEnumerator DelayedDestroyLaser(int time)
+    {
+        yield return new WaitForSeconds(time);
+        Destroy(gameObject);
+    }
+}
