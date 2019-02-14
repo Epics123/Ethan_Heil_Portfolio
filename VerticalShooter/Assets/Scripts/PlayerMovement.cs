81656c2ebb53cc50dc2a54ec858282bcd7537027
Ethan Heil <ethan.heil@mymail.champlain.edu>
2019-02-13 19:38:54 -0500

    Adding player movement

---
+++ VerticalShooter/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    float xMove = 0f;
+    float yMove = 0f;
+
+    public float speed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckInput();
+    }
+
+    void CheckInput()
+    {
+        xMove = (Input.GetAxis("Horizontal") * speed) * Time.deltaTime;
+        yMove = (Input.GetAxis("Vertical") * speed) * Time.deltaTime;
+        Move();
+    }
+
+    void Move()
+    {
+        transform.Translate(new Vector3(xMove, yMove, 0));
+    }
+}
