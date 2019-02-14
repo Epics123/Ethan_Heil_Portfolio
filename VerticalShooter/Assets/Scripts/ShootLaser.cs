4fb0edbaac4139e104d237dcd4353f0e98d895a8
Ethan Heil <ethan.heil@mymail.champlain.edu>
2019-02-13 20:09:29 -0500

    Adding the ability to fire lasers

---
+++ VerticalShooter/Assets/Scripts/ShootLaser.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootLaser : MonoBehaviour
+{
+
+    public GameObject laser;
+    public Transform laserSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckInput();
+    }
+
+    void CheckInput()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            FireLaser();
+        }
+    }
+
+    void FireLaser()
+    {
+        GameObject newLaser = Instantiate(laser, laserSpawn.position, laserSpawn.rotation);
+    }
+}
