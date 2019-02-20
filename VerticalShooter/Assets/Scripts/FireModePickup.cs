d63d073b47ca40009ff83b6aa89b144bfbf671a1
Ethan Heil <ethan.heil@mymail.champlain.edu>
2019-02-20 15:21:05 -0500

    Fixed rotaton on pickup

--- VerticalShooter/Assets/Scripts/FireModePickup.cs
+++ VerticalShooter/Assets/Scripts/FireModePickup.cs
@@ -8,7 +8,7 @@ public class FireModePickup : MonoBehaviour
     public ShootingMode shootMode;
     public int modeSelect;
     public float shootModeTime = 5f;
-    public float rotateSpeed = 40f;
+    public float movementSpeed = 3f;
 
     // Start is called before the first frame update
     void Start()
@@ -30,7 +30,7 @@ public class FireModePickup : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(0, 0, -rotateSpeed * Time.deltaTime));
+        transform.Translate(new Vector3(0, -movementSpeed * Time.deltaTime, 0));
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
