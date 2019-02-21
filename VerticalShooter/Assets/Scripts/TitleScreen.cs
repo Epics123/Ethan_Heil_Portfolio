ccbe2c9bfd80d66629d56894612df98312d10809
Ethan Heil <ethan.heil@mymail.champlain.edu>
2019-02-21 01:04:52 -0500

    Adding title screen

---
+++ VerticalShooter/Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreen : MonoBehaviour
+{
+    bool canHitKey;
+    // Start is called before the first frame update
+    void Start()
+    {
+        canHitKey = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckInput();
+    }
+
+    void CheckInput()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && canHitKey)
+        {
+            SceneManager.LoadSceneAsync(1);
+            canHitKey = false;
+        }
+    }
+}
