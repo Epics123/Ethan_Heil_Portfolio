9653b7e9e6f1ca4264bc4a3b063276f9165c2e84
Ethan Heil <ethan.heil@mymail.champlain.edu>
2019-02-28 13:18:33 -0500

    Enemies can now be destroyed on mouse click

---
+++ Midterm/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+
+    bool mouseOver = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseOver()
+    {
+        mouseOver = true;
+    }
+
+    private void OnMouseDown()
+    {
+        if(mouseOver)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
