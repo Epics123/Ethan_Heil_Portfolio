863c9550106a3e0cd706aa11da4fa4ba52457ba7
Ethan Heil <ethan.heil@mymail.champlain.edu>
2019-03-15 16:10:38 -0400

    Level title will slide in and out at the beginning of level 1 Adding pdf of level1Outline

---
+++ Platformer/Assets/Scripts/MoveUIElement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveUIElement : MonoBehaviour
+{
+    public Transform start;
+    public Transform end;
+    public float smoothTime;
+    bool hide = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(HideElement());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 desiredPos = Vector3.zero;
+        if (hide == false)
+        {
+            desiredPos = end.position;
+        }
+        else
+        {
+            desiredPos = start.position;
+        }
+
+        
+        Vector3 smoothPos = Vector3.Lerp(transform.position, desiredPos, Time.deltaTime / smoothTime);
+        transform.position = smoothPos;
+    }
+
+    IEnumerator HideElement()
+    {
+        yield return new WaitForSeconds(4f);
+        hide = true;
+    }
+}
