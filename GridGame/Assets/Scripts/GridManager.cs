dd96d0eeecefc2b246e13085f0a19168c57790de
Ethan Heil <ethan.heil@mymail.champlain.edu>
2019-03-27 19:48:32 -0400

    Grid now spawns at the start of the game

--- GridGame/Assets/Scripts/GridManager.cs
+++ GridGame/Assets/Scripts/GridManager.cs
@@ -11,7 +11,7 @@ public class GridManager : MonoBehaviour
     public float startY = 0f;
     public int rows;
     public int cols;
-    readonly float spacer = 0.1f;
+    readonly float spacer = 0.01f;
 
 
     private static GridManager instance;
@@ -19,12 +19,36 @@ public class GridManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        InitGridHolder();
+        BuildGrid();
+        instance = this;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void InitGridHolder()
+    {
+        gridHolder = new GameObject();
+        gridHolder.name = "Grid_Holder";
+        gridHolder.transform.position = new Vector2(startX, startY);
+    }
+
+    void BuildGrid()
+    {
+        for(int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                Square square = Instantiate(squarePrefab, gridHolder.transform);
+                Vector2 newPos = new Vector2(j + (spacer * j), i + (spacer * i));
+                square.transform.localPosition = newPos;
+                square.name = "Square_" + i + "_" + j;
+                square.gridPosition = new Vector2Int(i, j);
+            }
+        }
     }
 }
