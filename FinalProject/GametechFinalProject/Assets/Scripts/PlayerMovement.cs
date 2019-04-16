992b4c526afcbb560639b119d8439bfa36e786ba
Ethan Heil <ethan.heil@mymail.champlain.edu>
2019-04-16 16:00:56 -0400

    Adding horizontal movement for player. Adding Sprites: 	-Square.png

--- FinalProject/GametechFinalProject/Assets/Scripts/PlayerMovement.cs
+++ FinalProject/GametechFinalProject/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,111 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public Transform groundCheck;
+    public LayerMask ground;
+    public bool isGrounded = false;
+    public float xSpeed = 0.5f;
+    public float ySpeed = 7f;
+    public float jumpForce = 700f;
+    public float maxXSpeed = 10f;
+
+    Rigidbody2D rb2D;
+    float xMove = 0f;
+    float acc = 0.25f;
+    float frc = 0.3f;
+    bool shouldJump = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CheckInput();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+        CheckGround();
+    }
+
+    void CheckInput()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            if (xMove < 0)
+            {
+                xMove += (acc * xSpeed) * 2;
+                if (xMove >= -7.5f)
+                {
+                    xMove = 0f;
+                }
+            }
+            else
+            {
+                xMove += acc * xSpeed;
+                CheckMaxSpeed();
+            }
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            if (xMove > 0)
+            {
+                xMove -= (acc * xSpeed) * 2;
+                if (xMove <= 7.5f)
+                {
+                    xMove = 0f;
+                }
+            }
+            else
+            {
+                xMove -= acc * xSpeed;
+                CheckMaxSpeed();
+            }
+        }
+        else
+        {
+            xMove -= Mathf.Min(Mathf.Abs(xMove), frc) * Mathf.Sign(xMove);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            shouldJump = true;
+        }
+
+    }
+
+    void Move()
+    {
+        rb2D.velocity = new Vector2(xMove, rb2D.velocity.y);
+    }
+
+    void CheckMaxSpeed()
+    {
+        if (xMove >= maxXSpeed)
+        {
+            xMove = maxXSpeed;
+        }
+        if (xMove <= -maxXSpeed)
+        {
+            xMove = -maxXSpeed;
+        }
+    }
+
+    void CheckGround()
+    {
+        Collider2D col = Physics2D.OverlapCircle(groundCheck.position, 0.25f, ground);
+        if (col == null)
+        {
+            isGrounded = false;
+        }
+        else
+        {
+            isGrounded = true;
+        }
     }
 }
