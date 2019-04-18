c2ab6b9fda3674791e50ff01bb7a6118c2797084
Ethan Heil <ethan.heil@mymail.champlain.edu>
2019-04-18 01:08:02 -0400

    Adding scripts: 	-CameraController 	-WorldController
    Adding Shaders folder.
    Adding shader:
    	-WorldLook

---
+++ FinalProject/GametechFinalProject/Assets/Scripts/CameraController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Camera activeCam;
+    public Camera hiddenCam;
+
+
+    void Awake()
+    {
+        var rt = new RenderTexture(Screen.width, Screen.height, 24);
+        Shader.SetGlobalTexture("_WorldChange", rt);
+        hiddenCam.targetTexture = rt;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SwapCameras()
+    {
+        activeCam.targetTexture = hiddenCam.targetTexture;
+        hiddenCam.targetTexture = null;
+
+        var swapCam = activeCam;
+        activeCam = hiddenCam;
+        hiddenCam = swapCam;
+    }
+}
