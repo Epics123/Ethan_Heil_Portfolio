ff4b823d575c02921851e88e7c58947f3b14339a
Ethan Heil <eheil20@gmail.com>
2019-04-25 13:27:21 -0400

    Adding finished final exam

--- GameTechFinalExam/Assets/Scripts/GameManager.cs
+++ GameTechFinalExam/Assets/Scripts/GameManager.cs
@@ -2,15 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public LoadLevel loadLevel;
+    public Text text;
+    public static int timesPlayed = -1;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(SceneManager.GetActiveScene().name == "Start")
+        {
+            timesPlayed++;
+        }
+        text.text = timesPlayed.ToString();
     }
 
     // Update is called once per frame
